0a025aa75c3c52b4e085fe7b340f865f2153aca6
Amirreza Akbari <153768307+Amirrezaak83@users.noreply.github.com>
2024-02-02 20:46:07 +0330

    Add files via upload
    add login view

--- views.py
+++ views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.contrib.auth import login, authenticate
 from django.urls import reverse
 from .forms import *
-
+from django.contrib.auth.hashers import make_password
 
 
 
@@ -22,6 +22,38 @@ def register_view(request):
     else:
         form = register_form()
     return render(request, 'register.html', {'form': form})
+
+
+
+
+def your_home_view(request):
+    login_url = reverse('login')  # Use reverse to get the 'login' URL
+    return render(request, 'home.html', {'login_url': login_url})
+
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username= username, password= password)
+            if user is not None:
+                login(request, user)
+                return redirect('your_dashboard')
+            else:
+                messages.error(request, 'Invalid username or password')
+                return redirect('login')
+        else:
+            messages.error(request, f'invalid form submission,  error: {form.errors}')
+            return redirect('login')
+    else:
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
+
+
+
 
 
 
