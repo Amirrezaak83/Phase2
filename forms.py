7dc4bc6fc09790596e3f8d5579e9f5c5eccb9d8e
Amirreza Akbari <153768307+Amirrezaak83@users.noreply.github.com>
2024-02-02 20:44:06 +0330

    Add files via upload
    add login form

--- forms.py
+++ forms.py
@@ -1,7 +1,10 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, UsernameField
 from .models import *
 from django.core.exceptions import *
+from django.contrib.auth import authenticate
+
+
 
 class register_form(UserCreationForm): #user_creation_form have username and password by default
 
@@ -12,7 +15,7 @@ class register_form(UserCreationForm): #user_creation_form have username and pas
 
     class Meta:
         model = CustomUser
-        fields = ['user_type', 'first_name', 'last_name', 'username', 'password1', 'password2', 'email']
+        fields = ['first_name', 'last_name', 'username', 'password1', 'password2', 'email', 'user_type']
 
     def check_email(self):
         email = self.cleaned_data.get('email')
@@ -27,4 +30,22 @@ class register_form(UserCreationForm): #user_creation_form have username and pas
 
 
 
+class LoginForm(forms.Form):
+    username = UsernameField(widget=forms.TextInput(attrs={'autofocus': True}))
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(),
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is None:
+                raise forms.ValidationError("Invalid username or password.")
+
+        return cleaned_data
 
